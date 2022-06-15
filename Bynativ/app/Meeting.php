f47f257e9a27723acfbcdf031cc25478505f7d2a
arnaud <arnaud.lepichon-ext@insideboard.com>
2022-06-15 22:53:36 +0200

    feat: Meeting migration and validation

---
+++ Bynativ/app/Meeting.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Meeting extends Model
+{
+    protected $table = "meetings";
+
+    public $primaryKey = "id";
+
+    protected $fillable = [
+        'user',
+        'phone',
+        'email',
+        'time',
+        'message'
+    ];
+
+    protected $casts = [
+        'time' => 'date:m-d-Y'
+    ];
+}
---
+++ Bynativ/database/migrations/2022_06_13_190601_create_meetings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeetingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+            $table->string('user');
+            $table->string('phone')->nullable();
+            $table->string('email');
+            $table->date('time')->nullable();
+            $table->mediumText('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meetings');
+    }
+}
